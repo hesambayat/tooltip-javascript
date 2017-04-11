03bec39954bb492e5aa688592e9bfda2bbdb814b
hsmbyt <hesam.bayat@gmail.com>
2017-04-11 14:56:50 +0400

    Remove express.static

--- docs/server.js
+++ docs/server.js
@@ -3,7 +3,6 @@ var path = require('path');
 
 var PORT = 8080;
 var app = express();
-app.use('/dist', express.static(path.join(__dirname, '/dist')));
 app.use(express.static(__dirname));
 app.listen( PORT, function(err, res){
   console.log('Server listens to port ' + PORT + '...');
